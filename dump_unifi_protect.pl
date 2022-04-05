c5e793e688f906c6087860154151c00da48a5167
Mike Marion <mm@miguelito.org>
2022-04-05 15:01:18 -0700

    Saving script I used to dump my whole drive
    * Need to add in comments to explain

--- dump_unifi_protect.pl
+++ dump_unifi_protect.pl
@@ -7,16 +7,24 @@ use 5.018;
 use Cwd;
 use Data::Dumper;
 use File::Basename qw( dirname );
+use File::Copy;
 use File::Find;
 use File::Path qw (make_path remove_tree);
 use Getopt::Long;
+use IPC::Run3;
 use JSON;
+
 my $options;
 my $cameras;
 my $indir;
 my $outdir;
 my $infiles;
+my $totUbv = 0;
+my $totMp4 = 0;
 
+$| = 1;
+
+my $startTime = time();
 umask 022;
 
 GetOptions(
@@ -62,7 +70,6 @@ find (
 );
 
 foreach my $vfile (sort keys %$infiles) {
-#/mnt/ubnt-protect-in/unifi-os/unifi-protect/video/2022/01/13/E063DA3FEC8C_0_rotating_1642077111078.ubv
     my $name = $vfile;
     $name =~ s/.*\/video\//\/video\//;
     my $outputDir = "${outdir}$name";
@@ -74,9 +81,11 @@ foreach my $vfile (sort keys %$infiles) {
     }
     chdir($outputDir);
     # Use a tmp subdir for remux output so we can do another file for renames to camera names
-    mkdir("./tmp",775) or die "Failure to do mkdir(./tmp,775) $!\n";
-    chdir("tmp");
+    if ( ! -d 'tmp' ) { mkdir('tmp',0775) or die "Failure to do mkdir(./tmp,775) $!\n"; }
+    chdir('tmp');
+    $totUbv++;
     run_cmd("remux -with-audio $vfile");
+
     find (
             {
                 no_chdir    => 1,
@@ -84,24 +93,60 @@ foreach my $vfile (sort keys %$infiles) {
             },
             '.',
         );
+    chdir($outputDir);
+    remove_tree('tmp');
 }
 
+my $endTime = time();
+my $tookTime = $endTime - $startTime;
+
+print "
+Source ubv files processed:     $totUbv
+Destination mp4 files created:  $totMp4
+Time:                           $tookTime seconds
+";
+
+# End body
+
 sub run_cmd {
-    my $cmd = @_;
-    print $cmd;
+    my @cmd = @_;
+
+    debug("run_cmd: @cmd");
+    my ($stdin, $stdout, $stderr, $exit_code);
+    my $run = run3( @cmd, \$stdin, \$stdout, \$stderr);
+
 }
 
 sub renameFile {
     my $src = $File::Find::name;
     return unless ($src =~ /.*\.mp4$/);
 
-    print $src . "\n";
+    $src =~ s/\.\///g;
+    my $dst = $src;
+
+#/mnt/ubnt-protect-out/video/2021/06/01/FCECDA8FAD1D/2022-01-31T13.57.29-08.00.mp4
+    my $mac = substr($src,0,12);
+    (my $year, my $month, my $day) = ($src =~ /_(\d\d\d\d)-(\d\d)-(\d\d)T/);
+    my $ddir = "${outdir}/video/${year}/${month}/${day}/${mac}";
+    if (defined($cameras->{$mac})) {
+        $ddir = "${outdir}/video/${year}/${month}/${day}/$cameras->{$mac}";
+    }
+    if ( ! -d $ddir ) {
+        debug("make_path($ddir,{chmod => 0775})");
+        make_path($ddir, { chmod => 0775}) or die "Failure to do make_path($ddir,{chmod=>0775}) $!\n";
+    }
+    $dst =~ s/.*\d\d\d\d-\d\d-\d\dT//g;
+    $totMp4++;
+    my $fullDst = $ddir . "/" . $dst;
+    debug("Move $src, $fullDst");
+    move($src,$fullDst) or die "ERROR: $!\n";;
 }
 
 sub parseFile {
     my $filename = $File::Find::name;
 
     return unless ($filename =~ /.*\/video\/.*\.ubv$/);
+    return if ($filename =~ /_timelapse_/);
     $infiles->{$filename} = 1;
 }
 
